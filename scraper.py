378614591f8f230497802c4999a70ce1774e1ddc
hlurbur <h.lurbur@gmail.com>
2023-01-05 13:39:45 -0500

    try print pretify

--- scraper.py
+++ scraper.py
@@ -6,4 +6,8 @@ from bs4 import BeautifulSoup
 #The URL to scrape
 URL = "https://nwac.us/avalanche-forecast/#/all/"
 
+#the response
+r = requests.get(URL)
 
+soup = BeautifulSoup(r.content,'html5lib')
+print(soup.pretify())
