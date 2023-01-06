1c22ed64499c5d7d41a6eab7bdd4b4a3afd03ff5
hlurbur <h.lurbur@gmail.com>
2023-01-06 16:14:02 -0500

    still no work

--- scraper.py
+++ scraper.py
@@ -1,17 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
-# scrapes information from NWAC all forecasts page
-#The URL to scrape
-URL = "https://nwac.us/avalanche-forecast/#/all/"
+webdriver_path = ('/Users/hadassahlurbur/Desktop/Avvy-Savvy-Skiing/chromedriver')
 
-#the response
-r = requests.get(URL)
+driver = webdriver.Chrome(webdriver_path)
 
-#parse the HTML
-#soup = BeautifulSoup(r.content,'html5lib')
-soup = BeautifulSoup(r.text, 'html.parser')
+# the response
+r = driver.get('https://nwac.us/avalanche-forecast/#/olympics')
 
-#print the pretty version of the data
+soup = BeautifulSoup(driver.page_source, 'html5lib')
+
+
+#dangers = driver.find_elements_by_xpath('//td[@class="name"]')
+# # scrapes information from NWAC all forecasts page
+# #The URL to scrape
+# URL = "https://nwac.us/avalanche-forecast/#/olympics"
+
+# #the response
+# r = requests.get(URL)
+
+# #parse the HTML
+# soup = BeautifulSoup(r.content,'html5lib')
+# #soup = BeautifulSoup(r.text, 'html.parser')
+
+# #print the pretty version of the data
 print(soup.prettify())
