0f4826e2537b04dde2d69824dc81ec8e4d8b6f3d
hlurbur <h.lurbur@gmail.com>
2023-01-07 17:55:30 -0500

    works!!

--- scraper.py
+++ scraper.py
@@ -28,8 +28,18 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 # #print the pretty version of the data
 
 # found one
-print(soup.find(class_  = 'nac-contentPanel').find(class_ = 'nac-danger').find(class_ = 'nac-dangerLabel'))
-print(soup.findAll(class_ = 'nac-dangerLabel'))
+# print(soup.find(class_  = 'nac-contentPanel').find(class_ = 'nac-danger').find(class_ = 'nac-dangerLabel'))
+# print(soup.findAll(class_ = 'nac-dangerLabel'))
+
+dangers = soup.find(class_  = 'nac-contentPanel').findAll(class_ = 'nac-dangerLabel') #this is a list of HTML objects
+Above = dangers[0].text
+Near = dangers[1].text
+Below = dangers[2].text
+print("poop")
+
+# make a list of the URls, make a function that returns infor the URL
+
+
 #print(soup.find(class_  = 'nac-contentPanel').find_all(class_ = 'nac-danger').find_all(class_ = 'nac-dangerLabel'))
 #print(danger_text)
 #print(soup)
