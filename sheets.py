92c82ba48c99c89c1c6ce97cb1cc5f8ca4e1d9c6
hlurbur <h.lurbur@gmail.com>
2023-01-15 17:41:10 -0500

    push now

--- sheets.py
+++ sheets.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 
-url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSk4DAFdMfMObApX_jl4mseQM950yuxAuCxL2lJLe17Udi16JZOZ1KR1yq3UpagOkRGW7nHoESb6nTM/pubhtml"
+#url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSk4DAFdMfMObApX_jl4mseQM950yuxAuCxL2lJLe17Udi16JZOZ1KR1yq3UpagOkRGW7nHoESb6nTM/pubhtml"
 
-# https://docs.google.com/spreadsheets/d/1M4zSa9Osun8BaQos3dhi97FRa74uo5EJjaIh3NV7-3U/edit#gid=0
+#url = "https://docs.google.com/spreadsheets/d/1M4zSa9Osun8BaQos3dhi97FRa74uo5EJjaIh3NV7-3U/edit#gid=0"
 
 sheet_id = "1M4zSa9Osun8BaQos3dhi97FRa74uo5EJjaIh3NV7-3U"
 
-sheet_name = 'Ski Routes'
-url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-df = pd.read_csv(url)
+#sheet_name = 'Sheet1'
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?gid=0&format=csv"
+df = pd.read_csv(url,index_col=0)
 print(df.head())
