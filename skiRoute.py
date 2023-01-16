d778bb44cab7924b5eaa69b6d5271ee5af516c2c
hlurbur <h.lurbur@gmail.com>
2023-01-16 15:57:19 -0500

    made new skiRoute

---
+++ skiRoute.py
@@ -0,0 +1,11 @@
+
+
+class skiRoute:
+
+    __slots__ = ["region", "below_treeline", "near_treeling", "above_treeline", "avy_terrain", "avy_avoidable", "fun/10", "risk/10"]
+
+    def __init__(self):
+        """
+        Create a new skiRoute object. 
+        """
+        
