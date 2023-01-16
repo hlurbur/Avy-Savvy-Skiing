a72c47a1c99b37a05b23055905eec4ea93b43dab
hlurbur <h.lurbur@gmail.com>
2023-01-16 17:49:09 -0500

    working on things

--- sheets.py
+++ sheets.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import skiRoute
 
 
 #url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSk4DAFdMfMObApX_jl4mseQM950yuxAuCxL2lJLe17Udi16JZOZ1KR1yq3UpagOkRGW7nHoESb6nTM/pubhtml"
@@ -12,6 +13,8 @@ url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?gid=0&format=cs
 df = pd.read_csv(url,index_col=0)
 print(df.head())
 print(len(df))
+
+myRoutes = []
 
 for routeName, row in df.iterrows():
     print(routeName, row["Region"])
--- skiRoute.py
+++ skiRoute.py
@@ -4,11 +4,11 @@ class skiRoute:
 
    
 
-    def __init__(self, region, belowTreeline, nearTreeline, aboveTreeline, avyTerrain, avyAvoidable, funOften, riskOften):
+    def __init__(self, name, region, belowTreeline, nearTreeline, aboveTreeline, avyTerrain, avyAvoidable, funOften, riskOften):
         """
         Create a new skiRoute object. 
         """
-        
+        self._name = name
         self.region = region
         self._belowTreeline = belowTreeline
         self._nearTreeline = nearTreeline
@@ -18,6 +18,19 @@ class skiRoute:
         self._funOften = funOften
         self._riskOften = riskOften
 
+    def __str__(self) -> str:
+        """
+        return the string representation of a Ski Route
+        """
+        routeStr = "Name:" + self._name + ", Region:" + self._region + ", BelowTL:" + self._belowTreeline + ", Near TL:" + self._nearTreeline + ", AboveTL:" + self._aboveTreeline + ", AvyTrr:" + self._avyTerrain + ", Avoidable:" +  self._avyAvoidable + ", Fun/10:" + self._funOften + ", Risk/10:" + self._riskOften
+        return routeStr
+
+    def get_name(self):
+        """
+        get the name of the route
+        """
+        return self._name
+
     def get_region(self):
         """
         get the region that a route is in
@@ -26,35 +39,42 @@ class skiRoute:
 
     def get_belowTreeline(self):
         """
+        return T or F is the route crosses enters bwlow treeline elevation
         """
         return self._belowTreeline
 
     def get_nearTreeline(self):
         """
+        return T or F if the route croses near treeline elevation
         """
         return self._nearTreeline
     
     def get_aboveTreeline(self):
         """
+        return T of F if the route crosses above treeline elevation
         """
         return self._aboveTreeline
     
     def get_avyTerrain(self):
         """
+        return T of F if the route enters avalanche terrain
         """
         return self._avyTerrain
 
     def get_avyAvoidable(self):
         """
+        return T or F if it is possible to complete the route while avoiding avalanche terrain
         """
         return self._avyAvoidable
     
     def get_funOften(self):
         """
+        Return the route's fun score ranked out of 10
         """
         return self._funOften
 
     def get_riskOften(self):
         """
+        return the route's general risk score out of 10
         """
         return self._riskOften
