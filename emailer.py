65a54b24ce40468470002a44dbfcd82e66b45fb8
hlurbur <h.lurbur@gmail.com>
2023-01-24 20:56:09 -0500

    sends emails!

--- emailer.py
+++ emailer.py
@@ -1,1 +1,12 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+gmail_user = "AvyScraper@gmail.com"
+gmail_password = "vusezjvbwbnkpold"
+
+#create a secure SSL connection
+s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+s.login(gmail_user, gmail_password)
+s.sendmail(gmail_user, "mlurbur@gmail.com", "")
+s.quit()
